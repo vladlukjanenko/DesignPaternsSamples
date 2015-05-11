dd7b5704dfdcf8ac25027d3e1c6e43450ad93bf3
Vlad Lukjanenko <lukjanenko99@gmail.com>
2015-05-11 13:19:31 +0300

    Added MVC design pattern.

---
+++ src/behavioral/mvc/pattern/StudentView.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package behavioral.mvc.pattern;
+
+/**
+ * @author Vlad Lukjanenko
+ *
+ */
+public class StudentView {
+
+	public void printStudentDetails(String studentName, String studentRollNo) {
+
+		System.out.println("Student: ");
+		System.out.println("Name: " + studentName);
+		System.out.println("Roll No: " + studentRollNo);
+
+	}
+
+}
